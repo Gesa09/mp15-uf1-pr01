1e4f0249487dbef7d34ebd6942d2a40ec7de2cff
Gerard Sanchez Preto <144929923+Gesa09@users.noreply.github.com>
2023-12-11 13:03:45 +0100

    level 1 completed

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+def level1():
+    scene.set_background_image(assets.image("""
+        myImage
+    """))
+    tiles.set_current_tilemap(tilemap("""
+        nivel0
+    """))
+    tiles.place_on_tile(jugador, tiles.get_tile_location(7, 78))
+    controller.move_sprite(jugador, 75, 0)
+    jugador.ay = 800
+    info.start_countdown(60)
+def menu():
+    global jugador
+    info.set_life(3)
+    info.set_score(0)
+    tiles.set_current_tilemap(tilemap("""
+        nivel1
+    """))
+    jugador = sprites.create(img("""
+            . . . . . . f f f f . . . . . . 
+                    . . . . f f f 2 2 f f f . . . . 
+                    . . . f f f 2 2 2 2 f f f . . . 
+                    . . f f f e e e e e e f f f . . 
+                    . . f f e 2 2 2 2 2 2 e e f . . 
+                    . . f e 2 f f f f f f 2 e f . . 
+                    . . f f f f e e e e f f f f . . 
+                    . f f e f b f 4 4 f b f e f f . 
+                    . f e e 4 1 f d d f 1 4 e e f . 
+                    . . f e e d d d d d d e e f . . 
+                    . . . f e e 4 4 4 4 e e f . . . 
+                    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+                    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+                    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+                    . . . . . f f f f f f . . . . . 
+                    . . . . . f f . . f f . . . . .
+        """),
+        SpriteKind.player)
+    scene.camera_follow_sprite(jugador)
+    controller.move_sprite(jugador, 75, 75)
+    tiles.place_on_tile(jugador, tiles.get_tile_location(16, 12))
+
 def on_down_pressed():
-    animation.run_image_animation(mySprite,
+    animation.run_image_animation(jugador,
         [img("""
                 . . . . . . f f f f . . . . . . 
                         . . . . f f f 2 2 f f f . . . . 
@@ -77,7 +118,7 @@ def on_down_pressed():
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
 def on_right_pressed():
-    animation.run_image_animation(mySprite,
+    animation.run_image_animation(jugador,
         [img("""
                 . . . . . . f f f f f f . . . . 
                         . . . . f f e e e e f 2 f . . . 
@@ -155,7 +196,7 @@ def on_right_pressed():
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
 def on_left_pressed():
-    animation.run_image_animation(mySprite,
+    animation.run_image_animation(jugador,
         [img("""
                 . . . . f f f f f f . . . . . . 
                         . . . f 2 f e e e e f f . . . . 
@@ -232,41 +273,43 @@ def on_left_pressed():
         True)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
-def on_a_pressed():
-    if mySprite.is_hitting_tile(CollisionDirection.BOTTOM):
-        mySprite.vy = -350
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
-
-def on_countdown_end():
-    game.game_over(False)
-info.on_countdown_end(on_countdown_end)
-
-def on_overlap_tile(sprite, location):
-    scene.set_background_image(assets.image("""
-        myImage
-    """))
-    tiles.set_current_tilemap(tilemap("""
-        nivel0
-    """))
-    tiles.place_on_tile(mySprite, tiles.get_tile_location(7, 78))
-    controller.move_sprite(mySprite, 75, 0)
-    mySprite.ay = 800
-    info.start_countdown(60)
-scene.on_overlap_tile(SpriteKind.player,
-    sprites.dungeon.dark_ground_center,
-    on_overlap_tile)
-
-def on_overlap_tile2(sprite2, location2):
-    game.splash("Encuentra y supera los niveles de cada localización")
+def on_overlap_tile(sprite2, location2):
+    game.show_long_text("Busca y encuentra todas las bolas de dragón",
+        DialogLayout.BOTTOM)
     tiles.set_tile_at(location2, assets.tile("""
         myTile
     """))
 scene.on_overlap_tile(SpriteKind.player,
     sprites.dungeon.chest_closed,
+    on_overlap_tile)
+
+def on_a_pressed():
+    if jugador.is_hitting_tile(CollisionDirection.BOTTOM):
+        jugador.vy = -350
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def on_countdown_end():
+    perder_nivel()
+info.on_countdown_end(on_countdown_end)
+
+def on_overlap_tile2(sprite, location):
+    level1()
+scene.on_overlap_tile(SpriteKind.player,
+    sprites.dungeon.dark_ground_center,
     on_overlap_tile2)
 
+def on_overlap_tile3(sprite3, location3):
+    superar_nivel()
+scene.on_overlap_tile(SpriteKind.player,
+    sprites.dungeon.collectible_red_crystal,
+    on_overlap_tile3)
+
+def on_on_score():
+    game.game_over(True)
+info.on_score(1, on_on_score)
+
 def on_up_pressed():
-    animation.run_image_animation(mySprite,
+    animation.run_image_animation(jugador,
         [img("""
                 . . . . . . f f f f . . . . . . 
                         . . . . f f e e e e f f . . . . 
@@ -343,29 +386,29 @@ def on_up_pressed():
         True)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
-mySprite: Sprite = None
-tiles.set_current_tilemap(tilemap("""
-    nivel1
-"""))
-mySprite = sprites.create(img("""
-        . . . . . . f f f f . . . . . . 
-            . . . . f f f 2 2 f f f . . . . 
-            . . . f f f 2 2 2 2 f f f . . . 
-            . . f f f e e e e e e f f f . . 
-            . . f f e 2 2 2 2 2 2 e e f . . 
-            . . f e 2 f f f f f f 2 e f . . 
-            . . f f f f e e e e f f f f . . 
-            . f f e f b f 4 4 f b f e f f . 
-            . f e e 4 1 f d d f 1 4 e e f . 
-            . . f e e d d d d d d e e f . . 
-            . . . f e e 4 4 4 4 e e f . . . 
-            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f . . f f . . . . .
-    """),
-    SpriteKind.player)
-scene.camera_follow_sprite(mySprite)
-tiles.place_on_tile(mySprite, tiles.get_tile_location(16, 12))
-controller.move_sprite(mySprite, 75, 75)
+def on_life_zero():
+    game.game_over(False)
+info.on_life_zero(on_life_zero)
+
+def perder_nivel():
+    info.change_life_by(-1)
+    game.show_long_text("No has logrado superar el nivel", DialogLayout.BOTTOM)
+    tiles.set_current_tilemap(tilemap("""
+        nivel1
+    """))
+    tiles.place_on_tile(jugador, tiles.get_tile_location(6, 22))
+    restablecer_gravedad()
+def superar_nivel():
+    info.stop_countdown()
+    info.change_score_by(1)
+    game.show_long_text("Nivel TUTORIAL superado!!", DialogLayout.BOTTOM)
+    tiles.set_current_tilemap(tilemap("""
+        nivel4
+    """))
+    tiles.place_on_tile(jugador, tiles.get_tile_location(5, 26))
+    restablecer_gravedad()
+def restablecer_gravedad():
+    controller.move_sprite(jugador, 75, 75)
+    jugador.ay = 0
+jugador: Sprite = None
+menu()
--- main.ts
+++ main.ts
@@ -1,6 +1,40 @@
+function level1 () {
+    scene.setBackgroundImage(assets.image`myImage`)
+    tiles.setCurrentTilemap(tilemap`nivel0`)
+    tiles.placeOnTile(jugador, tiles.getTileLocation(7, 78))
+    controller.moveSprite(jugador, 75, 0)
+    jugador.ay = 800
+    info.startCountdown(60)
+}
+function menu () {
+    info.setLife(3)
+    info.setScore(0)
+    tiles.setCurrentTilemap(tilemap`nivel1`)
+    jugador = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    scene.cameraFollowSprite(jugador)
+    controller.moveSprite(jugador, 75, 75)
+    tiles.placeOnTile(jugador, tiles.getTileLocation(16, 12))
+}
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
-    mySprite,
+    jugador,
     [img`
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
@@ -76,7 +110,7 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
-    mySprite,
+    jugador,
     [img`
         . . . . . . f f f f f f . . . . 
         . . . . f f e e e e f 2 f . . . 
@@ -152,7 +186,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
-    mySprite,
+    jugador,
     [img`
         . . . . f f f f f f . . . . . . 
         . . . f 2 f e e e e f f . . . . 
@@ -226,29 +260,30 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite2, location2) {
+    game.showLongText("Busca y encuentra todas las bolas de dragón", DialogLayout.Bottom)
+    tiles.setTileAt(location2, assets.tile`myTile`)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        mySprite.vy = -350
+    if (jugador.isHittingTile(CollisionDirection.Bottom)) {
+        jugador.vy = -350
     }
 })
 info.onCountdownEnd(function () {
-    game.gameOver(false)
+    perder_nivel()
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundCenter, function (sprite, location) {
-    scene.setBackgroundImage(assets.image`myImage`)
-    tiles.setCurrentTilemap(tilemap`nivel0`)
-    tiles.placeOnTile(mySprite, tiles.getTileLocation(7, 78))
-    controller.moveSprite(mySprite, 75, 0)
-    mySprite.ay = 800
-    info.startCountdown(60)
+    level1()
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
-    game.splash("Encuentra y supera los niveles de cada localización")
-    tiles.setTileAt(location, assets.tile`myTile`)
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite3, location3) {
+    superar_nivel()
+})
+info.onScore(1, function () {
+    game.gameOver(true)
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
-    mySprite,
+    jugador,
     [img`
         . . . . . . f f f f . . . . . . 
         . . . . f f e e e e f f . . . . 
@@ -322,26 +357,27 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
-let mySprite: Sprite = null
-tiles.setCurrentTilemap(tilemap`nivel1`)
-mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-scene.cameraFollowSprite(mySprite)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(16, 12))
-controller.moveSprite(mySprite, 75, 75)
+info.onLifeZero(function () {
+    game.gameOver(false)
+})
+function perder_nivel () {
+    info.changeLifeBy(-1)
+    game.showLongText("No has logrado superar el nivel", DialogLayout.Bottom)
+    tiles.setCurrentTilemap(tilemap`nivel1`)
+    tiles.placeOnTile(jugador, tiles.getTileLocation(6, 22))
+    restablecer_gravedad()
+}
+function superar_nivel () {
+    info.stopCountdown()
+    info.changeScoreBy(1)
+    game.showLongText("Nivel TUTORIAL superado!!", DialogLayout.Bottom)
+    tiles.setCurrentTilemap(tilemap`nivel4`)
+    tiles.placeOnTile(jugador, tiles.getTileLocation(5, 26))
+    restablecer_gravedad()
+}
+function restablecer_gravedad () {
+    controller.moveSprite(jugador, 75, 75)
+    jugador.ay = 0
+}
+let jugador: Sprite = null
+menu()
